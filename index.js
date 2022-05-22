6032a661bf75fbf0cb1d10888fda24f049ddcbdb
REDUAN <reduan15-11413@diu.edu.bd>
2022-05-22 16:24:34 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r4qaf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Hello database")
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("Look manufature_server! I can code Node now!");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
