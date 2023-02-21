fe6abcffc4113f608a0c6a94914c75cb6dc8ba49
REDUAN <reduan15-11413@diu.edu.bd>
2023-02-21 12:55:53 +0600

    add:update vercel

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Look manufature_server! ");
+  res.send("Look manufature server! ");
 });
 
 app.listen(port, () => {
